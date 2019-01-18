a3d3506b0686adb0511d46786bbe091fc4d1191e
jananividhya <jananividhya123@gmail.com>
2019-01-18 14:50:04 +0530

    intialization matrix changes

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
-function makeArray(w, h) {
+function makeArray(row, column) {
     var matrixArray = [];
-    for(i = 0; i < h; i++) {
+    for(i = 0; i < row; i++) {
         matrixArray[i] = [];
-        for(j = 0; j < w; j++) {
+        for(j = 0; j < column; j++) {
             matrixArray[i][j] = 0;
         }
     }
     return matrixArray;
 }
 console.log(makeArray(3,3));
+console.log("enter a row:");
+console.log("enter the column:")
