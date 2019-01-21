5232ee08e2a4e7b5a09ad6894ff217e6580cfda2
karthikeyanrj <karthikeyanjeyabalann@gmail.com>
2019-01-21 11:50:19 +0530

    changed the testcases

--- index.test.js
+++ index.test.js
@@ -1,45 +1,52 @@
 const { TicTacToeGame } = require('./index');
 var assert = require('assert');
 
-let width = 3, height = 3;
+let len = 9;
 let arr = [["-","-","-"],["-","-","-"],["-","-","-"]];
 let tictactoeObj = new TicTacToeGame();
 describe('Checking with matrix', () => {
 
 it('should check the matrix length', () => {
     console.log("checking")
-     let res = tictactoeObj.makeArray(width, height);
-    assert.equal(res.length * res[0].length,(width * height));
+     let res = tictactoeObj.makeArray(len);
+    assert.equal(res.length * res[0].length,(len));
 
 });
 
 it ('should check the matrix initialization', () => {
-    let res = tictactoeObj.makeArray(width, height);
+    let res = tictactoeObj.makeArray(len);
     let count = 0;
-    for(let i = 0; i < height ; i++) {
-        for(let j = 0; j < width; j++){
-            if (res [i][j] == '-')
+    for(let i = 0; i < len ; i++)
+    {
+            if (res [i]== '-')
                 count ++;
             else
                 break;
-        }
+        
     }
-    assert.equal(count,(width * height));
+    assert.equal(count,(len));
 });
 });
 
 describe('Checking user inputs boundary conditions', () => {
-     let x , y;
+     let x ;
     it ('should check the user values is less than the matrix boundary', () => {
-             x = 2, y = 1;
-             console.log(x + " " + y)
-            assert.equal(tictactoeObj.checkInput(arr,x,y,width,height),true);
+             x = 2;
+             console.log(x)
+            assert.equal(tictactoeObj.checkInput(arr,x,len),true);
      });
 
      it('should check the user inputs is greater than the matrix boundary', () => {
-         x = 100, y = 9;
-         assert.equal(tictactoeObj.checkInput(arr,x,y,width,height),false);
+         x = 100;
+         assert.equal(tictactoeObj.checkInput(arr,x,len),false);
      });
+
+
+     it('should check the user inputs is working for negative values', () => {
+        x = -1;
+        assert.equal(tictactoeObj.checkInput(arr,x,len),false);
+    });
+
 
 });
 
